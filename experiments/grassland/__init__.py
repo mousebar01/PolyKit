05a7beaa754b6f3315b84b71fc5a329adcadb2f6
mousebar01 <mousebar01@163.com>
2026-08-28 15:25:41 +0800

    Add grassland experiment package

---
+++ experiments/grassland/__init__.py
@@ -0,0 +1,6 @@
+"""Blender grassland environment experiment inspired by lightweight game-world vegetation systems."""
+
+from .config import GrasslandConfig
+from .demo import build_grassland_demo
+
+__all__ = ["GrasslandConfig", "build_grassland_demo"]
