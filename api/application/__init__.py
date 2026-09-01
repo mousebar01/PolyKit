618ac0e1795a2915505a4a71fc6dd82292ba91d8
mousebar01 <mousebar01@163.com>
2026-09-01 14:58:38 +0800

    Export World application commands

--- api/application/__init__.py
+++ api/application/__init__.py
@@ -7,13 +7,23 @@ from .generate_asset import (
     compile_generate_asset_from_image_plan,
     compile_generate_asset_plan,
 )
+from .world import (
+    BuildWorldStructureCommand,
+    ComposeWorldCommand,
+    prepare_world_composition_run,
+    prepare_world_structure_run,
+)
 
 __all__ = [
+    "BuildWorldStructureCommand",
+    "ComposeWorldCommand",
     "GenerateAssetCommand",
     "GenerateAssetFromImageCommand",
     "PreparedExecution",
     "compile_generate_asset_from_image_plan",
     "compile_generate_asset_plan",
     "prepare_execution_run",
+    "prepare_world_composition_run",
+    "prepare_world_structure_run",
     "validate_execution_plan",
 ]
